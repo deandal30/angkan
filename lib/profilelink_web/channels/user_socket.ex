3c975d1905b6b80f8023be5d518601acdbc458fa
Daniel Eduard Andal <daniel_andal@medilink.com.ph>
2018-09-13 14:21:34 +0800

    Use production config from Heroku ENV variables and decrease socket timeout

--- lib/profilelink_web/channels/user_socket.ex
+++ lib/profilelink_web/channels/user_socket.ex
@@ -5,7 +5,8 @@ defmodule ProfilelinkWeb.UserSocket do
   # channel "room:*", ProfilelinkWeb.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+    timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
